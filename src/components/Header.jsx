c5f49a144c62767f11f6d9f5091809c450370f69
timabloom <timabloom@proton.me>
2022-07-18 15:05:07 +0200

    code consistency

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,30 @@
 import { Box, Flex, Link } from "@chakra-ui/react";
+import { Link as ReachLink } from "react-router-dom";
 import DarkMode from "./DarkMode";
 
 function Header() {
   return (
     <Box m="0 70px 0 70px">
-      <Flex justifyContent="space-between" align="center" p="30px 0 30px 0">
-        <Link w="110px" fontSize="xl" to="/">
+      <Flex justify="space-between" align="center" p="30px 0 30px 0">
+        <Link w="110px" fontSize="xl" as={ReachLink} to="/">
           My Portfolio
         </Link>
         <Box>
-          <Link mr={7} to="/">
+          <Link mr={7} as={ReachLink} to="/">
             Home
           </Link>
-          <Link mr={7} to="/About">
+          <Link mr={7} as={ReachLink} to="/About">
             About
           </Link>
-          <Link mr={7} to="/Projects">
+          <Link mr={7} as={ReachLink} to="/Projects">
             Projects
           </Link>
-          <Link mr={7} to="/Uses">
+          <Link mr={7} as={ReachLink} to="/Uses">
             Uses
           </Link>
-          <Link to="/Contact">Contact</Link>
+          <Link as={ReachLink} to="/Contact">
+            Contact
+          </Link>
         </Box>
         <DarkMode />
       </Flex>
