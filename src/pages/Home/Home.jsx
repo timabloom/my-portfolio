72dd7279b8fb351af522a4af239af474c2beeb97
timabloom <timabloom@proton.me>
2022-07-18 15:10:50 +0200

    created nice typing animation for home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,17 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
+import WelcomeAnimation from "./WelcomeAnimation";
 
 function Home() {
-  return <Box></Box>;
+  return (
+    <Box w="55%">
+      <Flex direction="column">
+        <WelcomeAnimation />
+        <Box p="70px" color="whiteAlpha.900" bg="#414141">
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+        </Box>
+      </Flex>
+    </Box>
+  );
 }
 
 export default Home;
---
+++ src/pages/Home/WelcomeAnimation.jsx
@@ -0,0 +1,199 @@
+import { Box, Text } from "@chakra-ui/react";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+import { useRecoilState } from "recoil";
+import { homeAnimation } from "../../Recoil/atom";
+
+const container = {
+  hidden: { display: "inline" },
+  visible: {
+    display: "inline",
+    transition: {
+      staggerChildren: 0.25,
+    },
+  },
+};
+
+const item = {
+  hidden: { display: "none" },
+  visible: {
+    display: "inline",
+  },
+};
+
+function WelcomeAnimation() {
+  const [typingAnimation, setTypingAnimation] = useState();
+  const [animationDone, setAnimationDone] = useRecoilState(homeAnimation);
+  const [textMarker, setTextMarker] = useState({
+    fontSize: "4xl",
+    position: "static",
+    top: 0,
+    borderLeft: "3px solid",
+    opacity: 1,
+    repeat: 1,
+    repeatDelay: 0,
+  });
+  const [markerDone, setMarkerDone] = useState(false);
+
+  useEffect(() => {
+    if (animationDone === false) {
+      setTimeout(() => {
+        setTextMarker({
+          fontSize: "18px",
+          position: "relative",
+          top: "19px",
+          borderLeft: "2px solid",
+          opacity: 1,
+          repeat: 1,
+          repeatDelay: 0,
+        });
+      }, "6560");
+      setTimeout(() => {
+        setTextMarker({
+          fontSize: "18px",
+          position: "relative",
+          top: "19px",
+          borderLeft: "2px solid",
+          opacity: [0, 0, 1, 1],
+          repeat: Infinity,
+          repeatDelay: 0.22,
+        });
+      }, "14200");
+      setAnimationDone(true);
+      setMarkerDone(true);
+      setTypingAnimation(
+        <motion.div variants={container} initial="hidden" animate="visible">
+          {[
+            "H",
+            "e",
+            "l",
+            "l",
+            "o",
+            "!",
+            " ",
+            "I",
+            " ",
+            "A",
+            "m",
+            <br />,
+            "T",
+            "i",
+            "m",
+            "o",
+            "t",
+            "h",
+            "y",
+            " ",
+            "M",
+            "a",
+            "g",
+            "e",
+            "e",
+            ",",
+            <br />,
+            "a",
+            "n",
+            "d",
+            " ",
+            "W",
+            "e",
+            "l",
+            "c",
+            "o",
+            "m",
+            "e",
+            " ",
+            "t",
+            "o",
+            " ",
+            "M",
+            "y",
+            " ",
+            "P",
+            "o",
+            "r",
+            "t",
+            "f",
+            "o",
+            "l",
+            "i",
+            "o",
+            ".",
+          ].map((letters, index) => {
+            if (index > 26) {
+              return (
+                <motion.span
+                  key={index}
+                  variants={item}
+                  style={{
+                    fontSize: "18px",
+                    position: "relative",
+                    top: "19px",
+                  }}
+                >
+                  {letters}
+                </motion.span>
+              );
+            } else {
+              return (
+                <motion.span
+                  key={index}
+                  variants={item}
+                  style={{ fontSize: "36px" }}
+                >
+                  {letters}
+                </motion.span>
+              );
+            }
+          })}
+        </motion.div>
+      );
+    } else {
+      setTypingAnimation(
+        <Box>
+          <Text fontSize="36px">
+            Hello! I Am
+            <br />
+            Timothy Magee,
+          </Text>
+          <Text display="inline" fontSize="18px">
+            and Welcome to My Portfolio.
+          </Text>
+        </Box>
+      );
+    }
+  }, []);
+
+  return (
+    <Box
+      h="300px"
+      ml="70px"
+      p="70px 70px 70px 0"
+      borderTop="1px solid"
+      fontSize={textMarker.fontSize}
+      fontWeight="bold"
+    >
+      {typingAnimation}
+      {markerDone ? (
+        <motion.span
+          animate={{ opacity: textMarker.opacity }}
+          transition={{
+            repeat: textMarker.repeat,
+            repeatDelay: textMarker.repeatDelay,
+          }}
+        >
+          <Text
+            display="inline"
+            borderLeft={textMarker.borderLeft}
+            position={textMarker.position}
+            top={textMarker.top}
+          ></Text>
+        </motion.span>
+      ) : (
+        <Text display="none" />
+      )}
+    </Box>
+  );
+}
+
+export default WelcomeAnimation;
