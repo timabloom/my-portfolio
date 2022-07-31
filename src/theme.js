eeceb53e0d4fc6c0745e1cfc521513669bbebde9
timabloom <timabloom@proton.me>
2022-07-31 21:47:55 +0200

    changed default colormode theme

--- src/theme.js
+++ src/theme.js
@@ -11,8 +11,8 @@ const colorMode = extendTheme({
 const styles = {
   global: (props) => ({
     body: {
-      color: mode("gray.800", "whiteAlpha.900")(props),
-      bg: mode("white", "#282923")(props),
+      color: mode("#1e1e1e", "white")(props),
+      bg: mode("white", "#1e1e1e")(props),
     },
   }),
 };
