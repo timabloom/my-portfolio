37e02d3486e93c5410a640967fd1a3757a672338
timabloom <timabloom@proton.me>
2022-07-31 17:46:04 +0200

    created simple sticky footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { HelmetProvider } from "react-helmet-async";
 import { BrowserRouter as Router } from "react-router-dom";
 import { RecoilRoot } from "recoil";
+import { Flex, Box } from "@chakra-ui/react";
 import Header from "./components/Header";
 import Main from "./components/Main";
 import Footer from "./components/Footer";
@@ -10,9 +11,13 @@ function App() {
     <HelmetProvider>
       <Router>
         <RecoilRoot>
-          <Header />
-          <Main />
-          <Footer />
+          <Flex direction="column" minH="100vh" alignItems="stretch">
+            <Header />
+            <Box flex={1}>
+              <Main />
+            </Box>
+            <Footer />
+          </Flex>
         </RecoilRoot>
       </Router>
     </HelmetProvider>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,11 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Text } from "@chakra-ui/react";
 
 function Footer() {
-  return <Box></Box>;
+  return (
+    <Box flexShrink={0} p="70px" color="whiteAlpha.900" bg="#414141">
+      <Text align="center">"Vestibulum accumsan placerat tellus"</Text>
+    </Box>
+  );
 }
 
 export default Footer;
