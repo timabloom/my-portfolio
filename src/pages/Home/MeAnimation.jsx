81fb98b19befba4f76d45d4d2f3ca7731c0ecc30
timabloom <timabloom@proton.me>
2022-08-08 19:46:45 +0200

    me typing animation created

--- src/pages/Home/MeAnimation.jsx
+++ src/pages/Home/MeAnimation.jsx
@@ -1,7 +1,39 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Image } from "@chakra-ui/react";
+import typingGif from "../../assets/typing.gif";
+import wavingGif from "../../assets/waving.gif";
+import typingStill from "../../assets/typing-still.png";
+import wavingStill from "../../assets/waving-still.png";
+import { useEffect, useState } from "react";
+import { useRecoilValue } from "recoil";
+import { homeAnimation } from "../../Recoil/homeAnimation/atom";
 
 function MeAnimation() {
-  return <Box w="45%" h="1000px" bg="orange.100"></Box>;
+  const [meAnimation, setMeAnimation] = useState();
+  const animationDone = useRecoilValue(homeAnimation);
+
+  useEffect(() => {
+    if (animationDone === false) {
+      setMeAnimation(typingStill);
+      setTimeout(() => {
+        setMeAnimation(typingGif);
+      }, "2000");
+      setTimeout(() => {
+        setMeAnimation(wavingGif);
+      }, "18400");
+    } else {
+      setMeAnimation(wavingStill);
+    }
+  }, []);
+
+  return (
+    <Box w="45%">
+      <Image
+        alt="Animation of me yyping on a keyboard"
+        src={meAnimation}
+        borderTop="solid 1px"
+      ></Image>
+    </Box>
+  );
 }
 
 export default MeAnimation;
