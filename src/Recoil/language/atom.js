af11178dd85388a657739b278d16b34cd7a69d66
timabloom <timabloom@proton.me>
2022-09-27 11:08:40 +0200

    local storage atom working properly

--- src/Recoil/language/atom.js
+++ src/Recoil/language/atom.js
@@ -1,8 +1,22 @@
 import { atom } from "recoil";
 
-const languageChoice = JSON.parse(localStorage.getItem("language"));
+const localStorageEffect =
+  (key) =>
+  ({ setSelf, onSet }) => {
+    const savedValue = localStorage.getItem(key);
+    if (savedValue != null) {
+      setSelf(JSON.parse(savedValue));
+    }
+
+    onSet((newValue, _, isReset) => {
+      isReset
+        ? localStorage.removeItem(key)
+        : localStorage.setItem(key, JSON.stringify(newValue));
+    });
+  };
 
 export const language = atom({
   key: "language",
-  default: languageChoice.language,
+  default: "GB",
+  effects: [localStorageEffect("language")],
 });
