10f081189fe2a4f3747e87ceb5016481598faf6d
timabloom <timabloom@proton.me>
2022-09-28 17:25:38 +0200

    chakra drawer fo menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,11 +9,16 @@ import {
   IconButton,
   useColorMode,
   useColorModeValue,
-  Slide,
-  Heading,
+  Drawer,
+  DrawerBody,
+  DrawerHeader,
+  DrawerOverlay,
+  DrawerContent,
+  useDisclosure,
+  DrawerFooter,
 } from "@chakra-ui/react";
 import { HamburgerIcon, CloseIcon } from "@chakra-ui/icons";
-import { useEffect, useState, useRef } from "react";
+import { useEffect, useState } from "react";
 import { Link as ReachLink } from "react-router-dom";
 import DarkMode from "./DarkMode";
 import { useRecoilState } from "recoil";
@@ -26,29 +31,13 @@ function Header() {
   const [flagDisabledSE, setFlagDisabledSE] = useState(false);
   const [flagDisabledGB, setFlagDisabledGB] = useState(true);
   const [isLargerThan1340] = useMediaQuery("(min-width: 1340px)");
-  const [isOpen, setIsOpen] = useState(false);
+  const { isOpen, onOpen, onClose } = useDisclosure();
   const { colorMode, toggleColorMode } = useColorMode();
-  const bg = useColorModeValue("#edf2f7", "#303030");
-  const refNav = useRef(null);
-
-  function handleNav() {
-    setIsOpen(!isOpen);
-  }
+  const bg = useColorModeValue("white", "#303030");
 
   useEffect(() => {
     handleFlagClicked(languageToggle);
   }, []);
-
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (refNav.current && !refNav.current.contains(event.target)) {
-        setIsOpen(false);
-      }
-    }
-    document.addEventListener("click", handleClickOutside, true);
-    return () =>
-      document.removeEventListener("click", handleClickOutside, true);
-  }, [refNav]);
 
   function handleFlagClicked(flag) {
     localStorage.setItem("language", JSON.stringify({ language: flag }));
@@ -141,139 +130,152 @@ function Header() {
         </>
       ) : (
         <Flex>
-          <Tooltip hasArrow label="English">
-            <Button
-              mr="10px"
-              onClick={() => handleFlagClicked("GB")}
-              disabled={flagDisabledGB}
-              _disabled
-            >
-              <Image
-                filter={grayFlagGB}
-                h="25px"
-                alt="EN"
-                src="http://purecatamphetamine.github.io/country-flag-icons/3x2/GB.svg"
-              />
-            </Button>
-          </Tooltip>
-          <Tooltip hasArrow label="Svenska">
-            <Button
-              onClick={() => handleFlagClicked("SE")}
-              disabled={flagDisabledSE}
-              _disabled
-            >
-              <Image
-                filter={grayFlagSE}
-                h="25px"
-                alt="SV"
-                src="http://purecatamphetamine.github.io/country-flag-icons/3x2/SE.svg"
-              />
-            </Button>
-          </Tooltip>
           <IconButton
             ml="40px"
-            icon={
-              isOpen === true ? (
-                <CloseIcon w="20px" h="20px" />
-              ) : (
-                <HamburgerIcon w="30px" h="30px" />
-              )
-            }
-            onClick={handleNav}
+            icon={<HamburgerIcon w="30px" h="30px" />}
+            onClick={onOpen}
           />
-          <Slide
-            direction="left"
-            in={isOpen}
-            style={{ zIndex: "10", width: "none" }}
-          >
-            <Box h="100vh" bg={bg} ref={refNav}>
-              <Heading h="100px" p="30px 20px 20px 30px">
-                {languageToggle === "GB" ? "Menu" : "Meny"}
-              </Heading>
-              <Link as={ReachLink} to="/">
-                <Button
-                  onClick={handleNav}
-                  p="20px 20px 40px 30px"
-                  bg={bg}
-                  display="block"
-                  textAlign="left"
-                  w="300px"
-                  borderRadius="none"
-                >
-                  {languageToggle === "GB" ? "Home" : "Hem"}
-                </Button>
-              </Link>
-              <Link as={ReachLink} to="/About">
-                <Button
-                  onClick={handleNav}
-                  p="20px 20px 40px 30px"
-                  bg={bg}
-                  display="block"
-                  textAlign="left"
-                  w="300px"
-                  borderRadius="none"
-                >
-                  {languageToggle === "GB" ? "About" : "Om mig"}
-                </Button>
-              </Link>
-              <Link as={ReachLink} to="/Projects">
-                <Button
-                  onClick={handleNav}
-                  p="20px 20px 40px 30px"
-                  bg={bg}
-                  display="block"
-                  textAlign="left"
-                  w="300px"
-                  borderRadius="none"
-                >
-                  {languageToggle === "GB" ? "Projects" : "Projekt"}
-                </Button>
-              </Link>
-              <Link as={ReachLink} to="/Uses">
-                <Button
-                  onClick={handleNav}
-                  p="20px 20px 40px 30px"
-                  bg={bg}
-                  display="block"
-                  textAlign="left"
-                  w="300px"
-                  borderRadius="none"
-                >
-                  {languageToggle === "GB" ? "Uses" : "Verktyg"}
-                </Button>
-              </Link>
-              <Link as={ReachLink} to="/Contact">
-                <Button
-                  onClick={handleNav}
-                  p="20px 20px 40px 30px"
-                  bg={bg}
-                  display="block"
-                  textAlign="left"
-                  w="300px"
-                  borderRadius="none"
-                >
-                  {languageToggle === "GB" ? "Contact" : "Kontakt"}
-                </Button>
-              </Link>
-              <Button
-                p="20px 20px 40px 30px"
-                bg={bg}
-                display="block"
-                textAlign="left"
-                w="300px"
-                borderRadius="none"
-                onClick={handleClick}
+          <Drawer isOpen={isOpen} placement="left" onClose={onClose}>
+            <DrawerOverlay />
+            <DrawerContent bg={bg}>
+              <Flex
+                ml="48px"
+                borderBottom="1px solid"
+                justifyContent="space-between"
+                align="center"
+                pr="24px"
               >
-                {colorMode === "light"
-                  ? languageToggle === "GB"
-                    ? "Dark Theme"
-                    : "Mörkt Tema"
-                  : languageToggle === "GB"
-                  ? "Light Mode"
-                  : "Ljust Tema"}
-              </Button>
-            </Box>
-          </Slide>
+                <DrawerHeader h="100px" p="35px 20px 35px 0">
+                  {languageToggle === "GB" ? "Menu" : "Meny"}
+                </DrawerHeader>
+                <IconButton
+                  icon={<CloseIcon w="20px" h="20px" />}
+                  onClick={onClose}
+                />
+              </Flex>
+              <DrawerBody p="0" pl="48px">
+                <Link as={ReachLink} to="/">
+                  <Button
+                    _hover={bg}
+                    _active={bg}
+                    onClick={onClose}
+                    mt="10px"
+                    p="20px 20px 40px 0"
+                    bg={bg}
+                    display="block"
+                    textAlign="left"
+                  >
+                    {languageToggle === "GB" ? "Home" : "Hem"}
+                  </Button>
+                </Link>
+                <Link as={ReachLink} to="/About">
+                  <Button
+                    _hover={bg}
+                    _active={bg}
+                    onClick={onClose}
+                    p="20px 20px 40px 0"
+                    bg={bg}
+                    display="block"
+                    textAlign="left"
+                  >
+                    {languageToggle === "GB" ? "About" : "Om mig"}
+                  </Button>
+                </Link>
+                <Link as={ReachLink} to="/Projects">
+                  <Button
+                    _hover={bg}
+                    _active={bg}
+                    onClick={onClose}
+                    p="20px 20px 40px 0"
+                    bg={bg}
+                    display="block"
+                    textAlign="left"
+                  >
+                    {languageToggle === "GB" ? "Projects" : "Projekt"}
+                  </Button>
+                </Link>
+                <Link as={ReachLink} to="/Uses">
+                  <Button
+                    _hover={bg}
+                    _active={bg}
+                    onClick={onClose}
+                    p="20px 20px 40px 0"
+                    bg={bg}
+                    display="block"
+                    textAlign="left"
+                  >
+                    {languageToggle === "GB" ? "Uses" : "Verktyg"}
+                  </Button>
+                </Link>
+                <Link as={ReachLink} to="/Contact">
+                  <Button
+                    _hover={bg}
+                    _active={bg}
+                    onClick={onClose}
+                    p="20px 20px 40px 0"
+                    bg={bg}
+                    display="block"
+                  >
+                    {languageToggle === "GB" ? "Contact" : "Kontakt"}
+                  </Button>
+                </Link>
+                <Button
+                  _active={bg}
+                  _hover={{ textDecoration: "underline" }}
+                  p="20px 20px 40px 0"
+                  bg={bg}
+                  display="block"
+                  textAlign="left"
+                  onClick={handleClick}
+                  w="100%"
+                >
+                  {colorMode === "light"
+                    ? languageToggle === "GB"
+                      ? "Dark Theme"
+                      : "Mörkt Tema"
+                    : languageToggle === "GB"
+                    ? "Light Mode"
+                    : "Ljust Tema"}
+                </Button>
+              </DrawerBody>
+              <DrawerFooter>
+                <DrawerHeader minW="150px">
+                  {languageToggle === "GB" ? "Language:" : "Språk:"}
+                </DrawerHeader>
+                <Tooltip hasArrow label="English">
+                  <Button
+                    mr="10px"
+                    onClick={() => handleFlagClicked("GB")}
+                    disabled={flagDisabledGB}
+                    _disabled
+                    p="0 12px 0 12px"
+                  >
+                    <Image
+                      filter={grayFlagGB}
+                      h="25px"
+                      alt="EN"
+                      src="http://purecatamphetamine.github.io/country-flag-icons/3x2/GB.svg"
+                    />
+                  </Button>
+                </Tooltip>
+                <Tooltip hasArrow label="Svenska">
+                  <Button
+                    onClick={() => handleFlagClicked("SE")}
+                    disabled={flagDisabledSE}
+                    _disabled
+                    p="0 12px 0 12px"
+                  >
+                    <Image
+                      filter={grayFlagSE}
+                      h="25px"
+                      alt="SV"
+                      src="http://purecatamphetamine.github.io/country-flag-icons/3x2/SE.svg"
+                    />
+                  </Button>
+                </Tooltip>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
         </Flex>
       )}
     </Flex>
