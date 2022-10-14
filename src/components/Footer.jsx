91085b7400742595f3e60b80af86bf938cd26d38
timabloom <timabloom@proton.me>
2022-10-14 14:54:47 +0200

    added cc license

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,11 @@
-import { Box, Link, Text, Flex, useColorModeValue } from "@chakra-ui/react";
+import {
+  Box,
+  Link,
+  Text,
+  Flex,
+  useColorModeValue,
+  Image,
+} from "@chakra-ui/react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
@@ -14,27 +21,90 @@ function Footer() {
       <Box mb={5} color={color}>
         <Link
           mr={6}
-          href="https://www.linkedin.com/in/timothy-magee-b3170424a"
+          href="https://www.linkedin.com/in/timothy-magee-b3170424a/"
           _hover={{ color: "#0a66c2" }}
         >
           <FontAwesomeIcon icon={faLinkedin} size="2x" fixedWidth />
         </Link>
         <Link
           mr={6}
-          href="https://github.com/timabloom"
+          href="https://github.com/cme-timothy?tab=repositories/"
           _hover={{ opacity: "70%" }}
         >
           <FontAwesomeIcon icon={faGithub} size="2x" fixedWidth />
         </Link>
         <Link href="mailto:timabloom@proton.me" _hover={{ color: "#ffd43b" }}>
           <FontAwesomeIcon icon={faEnvelope} size="2x" fixedWidth />
-        </Link> 
+        </Link>
       </Box>
-      <Text>
-        {languageToggle === "GB"
-          ? "Vestibulum accumsan placerat tellus"
-          : "Morbi cursus bibendum vestibulum."}
-      </Text>
+      {languageToggle === "GB" ? (
+        <Box align="center">
+          <Text>Copyright © 2022 Timothy Magee</Text>
+          <Link
+            rel="license"
+            href="http://creativecommons.org/licenses/by-sa/4.0/"
+          >
+            <Image
+              m="7px"
+              alt="Creative Commons Licence"
+              src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png"
+            />
+          </Link>
+          <Text>
+            This work is licensed under a{" "}
+            <Link
+              color="cyan.400"
+              rel="license"
+              href="http://creativecommons.org/licenses/by-sa/4.0/"
+            >
+              Creative Commons Attribution-ShareAlike 4.0 International License.
+            </Link>
+          </Text>
+          <Text>
+            The source code for this website can be found on{" "}
+            <Link
+              color="cyan.400"
+              rel="external"
+              href="https://github.com/timabloom/my-portfolio/"
+            >
+              GitHub.
+            </Link>
+          </Text>
+        </Box>
+      ) : (
+        <Box align="center">
+          <Link
+            rel="license"
+            href="https://creativecommons.org/licenses/by-sa/4.0/deed.sv/"
+          >
+            <Image
+              mb="7px"
+              alt="Creative Commons Licence"
+              src="https://i.creativecommons.org/l/by-sa/4.0/80x15.png"
+            />
+          </Link>
+          <Text>
+            Detta verk är licensierat under en{" "}
+            <Link
+              color="cyan.400"
+              rel="license"
+              href="https://creativecommons.org/licenses/by-sa/4.0/deed.sv/"
+            >
+              Creative Commons Erkännande-DelaLika 4.0 Internationell Licens.
+            </Link>
+          </Text>
+          <Text>
+            Källkoden för den här hemsidan finns på{" "}
+            <Link
+              color="cyan.400"
+              rel="external"
+              href="https://github.com/timabloom/my-portfolio/"
+            >
+              GitHub.
+            </Link>
+          </Text>
+        </Box>
+      )}
     </Flex>
   );
 }
