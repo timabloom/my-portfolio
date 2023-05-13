4ab9cf13e3632e0f6eadfef05648e5a351b0cc20
timabloom <timabloom@proton.me>
2023-05-13 11:35:32 +0200

    better naming conventions

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -9,9 +9,9 @@ function Main() {
     <>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/Projects" element={<Projects />} />
-        <Route path="/About" element={<About />} />
-        <Route path="/Contact" element={<Contact />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
     </>
   );
